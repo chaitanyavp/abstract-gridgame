930f25b3a5ac298cb099427d9cdd1d6204abad00
chaitanyavp <chaitanya1998@gmail.com>
2017-08-17 17:29:25 -0400

    Made imports more efficient

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,7 +1,18 @@
-import javax.swing.*;
-import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Minesweeper extends GridGame {
 
